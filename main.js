77e33ffc69fad4e0d5f6dab315c15837e2c59896
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-04-27 12:14:18 +0100

    update main

--- main.js
+++ main.js
@@ -57,6 +57,11 @@ spaceBar.onDown.add(this.jump, this);
 //Timer for pipes
 
 this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    
+//Score
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0", 
+        { font: "30px Arial", fill: "#ffffff" });
 
 },
 
