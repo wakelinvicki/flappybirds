bfcdef44077973e13dd66b1404a13b92ef804d54
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-04-28 11:31:51 +0100

    update main

--- main.js
+++ main.js
@@ -84,6 +84,10 @@ this.restartGame();
 //calls the restartGame function each time the bird dies
 game.physics.arcade.overlap(
   this.bird, this.pipes, this.restartGame, null, this);
+    
+//Slowly rotate the bird downward, up to a certain point
+if (this.bird.angle < 20)
+    this.bird.angle += 1;
 
 },
     
@@ -94,6 +98,15 @@ jump: function() {
 //Add a vertial velocity to the bird
 
 this.bird.body.velocity.y = -350;
+    
+//Create an animation on the bird 
+var animation = game.add.tween(this.bird);
+    
+//Change the angle of the bird to -20 in 100 milliseconds
+animation.to({angle: -20}, 100);
+    
+//Add the start animation
+animation.start();
 
 },
     
